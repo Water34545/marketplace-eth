bf05e40203bcfa02a74e97c8f73cc9329b125993
Anton Gimpel <water34545@yandex.ru>
2022-04-12 20:49:18 +0400

    tailwind integration

--- pages/index.js
+++ pages/index.js
@@ -2,5 +2,18 @@ import Head from 'next/head'
 import Image from 'next/image'
 
 export default function Home() {
-  return <>hi!</>
+  return <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+  <div className="shrink-0">
+    <Image 
+      className="h-12 w-12" 
+      src="/vercel.svg" 
+      alt="ChitChat Logo" 
+      width={50}
+      height={50}/>
+  </div>
+  <div>
+    <div className="text-xl font-medium text-black">ChitChat</div>
+    <p className="text-slate-500">You have a new message!</p>
+  </div>
+</div>
 }
