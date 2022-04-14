a222f93866b73d6863b1f1f9772465f8501db55f
Anton Gimpel <water34545@yandex.ru>
2022-04-14 12:05:25 +0400

    adds data to courses list

--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,28 @@ import {BaseLayout} from '@components/layout';
 import {WalletBar, EthRates} from '@components/web3';
 import {OrderCard} from '@components/order';
 import {CourseList} from '@components/course';
+import {getAllCourses} from '@content/courses/fetcher';
 
-const Home = () => {
+const Home = ({courses}) => {
   return <>
     <Hero/>
     <Breadcrumbs/>
     <WalletBar/>
     <EthRates/>
     <OrderCard/>
-    <CourseList/>
+    <CourseList courses={courses}/>
   </>
 };
+
+export const getStaticProps = () => {
+  const {data} = getAllCourses();
+
+  return {
+    props: {
+      courses: data
+    }
+  }
+}
 
 Home.Layout = BaseLayout;
 
