0500ccdb7fd35a02a240b1fa4244af3197dd009e
Anton Gimpel <water34545@yandex.ru>
2022-05-01 13:42:57 +0400

    deploy to goerli test network

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,6 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const keys = require("./keys.json");
+
 module.exports = {
   contracts_build_directory: "./public/contracts",
   networks: {
@@ -5,6 +8,22 @@ module.exports = {
      host: "127.0.0.1",
      port: 7545,
      network_id: "*",
+    },
+    goerli: {
+      provider: () =>
+        new HDWalletProvider({
+          mnemonic: {
+            phrase: keys.MNEMONIC
+          },
+          providerOrUrl: `https://goerli.infura.io/v3/${keys.INFURA_PROJECT_ID}`,
+          addressIndex: 0
+        }),
+      network_id: 5,
+      gas: 8000000, // Gas Limit, How much gas we are willing to spent
+      gasPrice: 21000000000, // how much we are willing to spent for unit of gas
+      confirmations: 2, // number of blocks to wait between deployment
+      timeoutBlocks: 200, // number of blocks before deployment times out
+      networkCheckTimeoutnetworkCheckTimeout: 10000
     }
   },
   compilers: {
@@ -13,3 +32,6 @@ module.exports = {
     }
   }
 };
+
+//NEXT_PUBLIC_TARGET_CHAIN_ID=1337
+//NEXT_PUBLIC_NETWORK_ID=5777 
